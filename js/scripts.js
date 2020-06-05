a8d5b9f22fd750ef90dc14c5181710d565508c3a
Jeff Kim <kim2jy@hotmail.com>
2020-06-05 11:03:44 -0700

    Change experience question to drop-down and add branching to result in JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,19 @@ $(document).ready(function() {
     const appType = $("input:radio[name=app-type]:checked").val();
     const favMovie = $("input#favorite-movie").val();
 
-    if (experience) {
-      $("result").text("Please enter your programming experience as a number.");
-    }
-    else if (experience === 0) {
+    if (experience === 1) {
       $("#result").text("C++");
-      $("#explanation").text("Many object-oriented programming languages are based on C++ and the knowledge you gain will provide a good foundation for your programming journey.")
+      $("#explanation").text("Many object-oriented programming languages are based on C++ and the knowledge you gain will provide a good foundation for your programming journey.");
     }
-    // else {
-    //   if (product)
-    // }
-  });
+    else if (experience > 1) {
+      if (product === 1 || product === 2 && sourceType === "open") {
+        $("#result").text("JavaScript");
+        $("#explanation").text("JavaScript will help you learn how to make web pages dynamic and interactive and has many elements in common with other programming languages. It's a solid choice for any programmer because of how widely it is used.");
+      }
+      else if (product ===1 || product === 2 && sourceType === "closed") {
+        $("#result").text("C#");
+        $("#explanation").text("C# is a programming language that is developed and controlled by Microsoft Corporation and is widely used in web-based applications as well as Windows desktop apps.");
+      }
+    }
+  }
 });
